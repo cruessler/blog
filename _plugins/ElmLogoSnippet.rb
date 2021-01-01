1dd8f8c6dcb5bad793e5bb8e141afe941df004ad
Christoph Rüßler <christoph.ruessler@mailbox.org>
2021-01-01 13:40:29 +0100

    Create plugin for Logo snippets

---
+++ _plugins/ElmLogoSnippet.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  class ElmLogoSnippetBlock < Liquid::Block
+
+    def render(context)
+      snippet = super.strip
+      encoded_snippet = URI.encode_www_form_component(snippet)
+      link = "#{ENV['ELM_LOGO_URL']}?initialCommandLine=#{encoded_snippet}"
+
+      "```
+#{snippet}
+```
+
+<a href=\"#{link}\">Open snippet in elm-logo (in elm-logo, click ”Run” to run it)</a>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('elm_logo_snippet', Jekyll::ElmLogoSnippetBlock)
